b508f8a7ac5ca505ba3cf55804d4b7359d1950fb
anvar0522 <anvargehe@gmail.com>
2022-02-14 20:44:50 +0500

    Initial Commit

---
+++ JSon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSon
+//
+//  Created by anvar on 14/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
